aabed67b430cf65a58e3c511d221bb83da7caf70
dborhan <89890381+dborhan@users.noreply.github.com>
2022-10-31 13:07:11 -0400

    Completing Challenges B and C

--- app/Unemployment.py
+++ app/Unemployment.py
@@ -4,6 +4,8 @@
 import os
 import json
 from pprint import pprint
+from statistics import mean
+from plotly.express import line
 
 
 import requests
@@ -29,3 +31,27 @@ print("-------------------------")
 print("LATEST UNEMPLOYMENT RATE:")
 #print(data[0])
 print(f"{data[0]['value']}%", "as of", data[0]["date"])
+
+
+# Challenge B
+# What is the average unemployment rate for all months during this calendar year?
+# ... How many months does this cover?
+
+this_year = [d for d in data if "2022-" in d["date"]]
+
+rates_this_year = [float(d["value"]) for d in this_year]
+#print(rates_this_year)
+
+print("-------------------------")
+print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
+print("NO MONTHS:", len(this_year))
+
+# Challenge C
+# Plot a line chart of unemployment rates over time.
+
+
+dates = [d["date"] for d in data]
+rates = [float(d["value"]) for d in data]
+
+fig = line(x=dates, y=rates, title="United States Unemployment Rate over time", labels= {"x": "Month", "y": "Unemployment Rate"})
+fig.show()
